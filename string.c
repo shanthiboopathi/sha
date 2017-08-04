dbcadee54921474f712b2a0cad2a412ed236b53f
shanthiboopathi <30317727+shanthiboopathi@users.noreply.github.com>
2017-08-04 16:40:10 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ #define MAX_SIZE 100 //Maximum size of the string
+ int main()
+{
+char string[MAX_SIZE];
+int i, words;
+ printf("Enter any string: ");
+gets(string);
+ i = 0;
+words = 1;
+while(string[i]!='\0')
+ {
+if(string[i]==' ' || string[i]=='\n' || string[i]=='\t')
+{
+words++;
+}
+i++;
+}
+printf("Total number of words = %d\n", words);
+return 0;
+} 
